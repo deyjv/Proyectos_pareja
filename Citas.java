7d69c451f8d4ff3d924d943a8e0226b58a693a49
deyjv <deisyjimenez0502@gmail.com>
2025-09-02 19:41:07 -0600

    subi mi trabajo

---
+++ Citas.java
@@ -0,0 +1,52 @@
+public class Citas{
+	//Atributos
+	private String nombre;
+	private String cedula;
+	private String lugar ;
+	private String motivo;
+
+	//metodos construtor por defecto
+	public Citas (){}
+	public Citas (String nombre, String cedula, String lugar, String motivo){
+		this.nombre = nombre;
+		this.cedula = cedula;
+		this.lugar = lugar;
+		this.motivo = motivo;
+	}
+public void setNombre(String nombre){
+	this.nombre = nombre;
+}
+public String getNombre(){
+	return nombre;
+}
+public void setCedula(String cedula){
+	this.cedula = cedula;
+}
+public String getCedula(){
+	return cedula;
+}
+public void setLugar(String lugar){
+	this.lugar = lugar;
+}
+public String getLugar(){
+	return lugar;
+}
+public void setMotivo(String motivo){
+	this.motivo = motivo;
+}
+public String getmotivo(){
+	return motivo;
+}
+
+public double cobrarConsulta(int main_consulta){
+		if(main_consulta <=10)
+			return 10000.00;
+		}
+		if(main_consulta >10 && main_consulta <25){
+			return 25000.00;
+		}
+
+
+
+
+}
